4bc1fd88b5ae73fd02c45d188bbe0e68d1cf98c1
Mehdi682007 <123469029+Mehdi682007@users.noreply.github.com>
2024-08-17 17:55:20 +0330

    Update setup-socat.sh

--- setup-socat.sh
+++ setup-socat.sh
@@ -8,18 +8,8 @@ read -p "Enter the IPv6 address you want to forward to: " ipv6
 sudo apt update
 sudo apt install -y socat
 
-# بررسی و اضافه کردن #!/bin/bash به ابتدای /etc/rc.local در صورت عدم وجود
-if ! grep -q "^#!/bin/bash" /etc/rc.local; then
-    echo "Adding #!/bin/bash to the top of /etc/rc.local"
-    sudo sed -i '1i #!/bin/bash' /etc/rc.local
-else
-    echo "#!/bin/bash already present in /etc/rc.local"
-fi
-
-# حذف exit 0 در صورت وجود، برای اضافه کردن دستورات جدید
-sudo sed -i '/exit 0/d' /etc/rc.local
-
-# اضافه کردن دستور socat به /etc/rc.local
+# بازنویسی /etc/rc.local با افزودن #!/bin/bash به بالا
+echo "#!/bin/bash" | sudo tee /etc/rc.local
 echo "socat TCP4-LISTEN:${port},fork TCP6:[${ipv6}]:${port},ipv6only=1 &" | sudo tee -a /etc/rc.local
 echo "exit 0" | sudo tee -a /etc/rc.local
 
